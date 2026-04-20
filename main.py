7ac9a1b7d31f9c57dd6f125b2656d7e317a69201
d3pr <yoaiev0@gmail.com>
2026-04-20 08:57:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def send_periodic_msg(token, name):
                 print(f"⚠️ {name} message error: {e}")
         
         # Wait 2 hours (7200 seconds)
-        time.sleep(8) 
+        time.sleep(11) 
 
 def vc_locker(token, name):
     if not token:
